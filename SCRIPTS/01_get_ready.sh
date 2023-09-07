daf92f247fdee8f4eb6ed5c8d479f522611ef8bf
SFntbks <66406004+SFntbks@users.noreply.github.com>
2023-09-07 17:56:05 +0800

    Update 01_get_ready.sh

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 latest_release="$(curl -s https://github.com/openwrt/openwrt/tags | grep -Eo "v[0-9\.]+\-*r*c*[0-9]*.tar.gz" | sed -n '/[2-9][0-9]/p' | sed -n 1p | sed 's/.tar.gz//g')"
-git clone --single-branch -b ${latest_release} https://github.com/openwrt/openwrt openwrt_release
+git clone --single-branch -b v22.03.5 https://github.com/openwrt/openwrt openwrt_release
 git clone --single-branch -b openwrt-22.03 https://github.com/openwrt/openwrt.git openwrt
 rm -f ./openwrt/include/version.mk
 rm -f ./openwrt/include/kernel.mk
